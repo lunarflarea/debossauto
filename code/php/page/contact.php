9b7fff602575dab39ee3988fd1f3ca2aae635235
dede95760 <bastienauffret95760@gmail.com>
2021-04-26 17:27:58 +0200

    v17

--- code/php/page/contact.php
+++ code/php/page/contact.php
@@ -2,130 +2,120 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"> <!-- je link les icones de materialize si besion neccessaire -->
-    <link rel="stylesheet" href="../../css/style.css"> <!-- je link mon css afin de changer les parametre de ma page -->
-    <link rel="stylesheet" href="../../css/materialize.min.css"  media="screen,projection"/> <!-- j'utilise materialize pour mon site -->
-    <title></title>    
-   <?php session_start(); ?>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <!-- linking des icônes de Materialize si besoin -->
+    <link rel="stylesheet" href="../../css/style.css">
+    <!-- linking le fichier .css afin de changer les paramètres de ma page -->
+    <link rel="stylesheet" href="../../css/materialize.min.css"  media="screen,projection"/>
+    <!-- j'utilise Materialize pour mon site -->
+    <title>Contact</title>
+    <meta name="description" content="Vous pouvez contacter un technicien à tout moment si votre véhicule a un problème"> <!-- Ainsi qu'une description -->
+    <meta name="keywords" content="Debossauto, debosselage, dsp, automobile, technique">
+    <link rel="shortcut icon" href="../../../assets/logo.png"> <!-- j'ajoute un logo sur l'onglet du site  -->
+    <?php session_start(); ?>
   </head>
   <body>
-  <div class="blue-grey darken-3">
-    <header>
-      <?php $dep = '';?>
-     <?php  include '../content/nav.php';?>
-    </header>
-
-
-    <div class="row">
-      <div class="container center-align">
-        <h1 class="white-text">Une bosse sur votre carrosserie? Contactez-nous!</h1>
-      </div>
-    </div>
-    <?php
-      if (isset($_SESSION['contact_send_error'])) {
-    ?>
-    <div class="row">
-      <div class="container center-align">
-        <p class="red-text">
-          <?php echo($_SESSION['contact_send_error']);
-          unset($_SESSION['contact_send_error']); ?>
-        </p>
-      </div>
-    </div>
-    <?php }; ?>
-
-    <form action="../content/contactsend.php" method="post">
-    <div class="container">
+    <div class="blue-grey darken-3">
+      <header>
+        <?php $dep = '';?>
+        <?php  include '../content/nav.php';?>
+      </header>
       <div class="row">
-        <div class="input-field col s6">
-          <input name="nom" placeholder="Ex: DUBOIS" id="first_name" type="text" class="validate" class="white-text">
-          <label for="first_name">Nom</label>
-        </div>
-        <div class="input-field col s6">
-          <input name="prenom" placeholder="Ex: Paul" id="last_name" type="text" class="validate" class="white-text">
-          <label for="last_name">Prénom</label>
+        <div class="container center-align">
+          <h1 class="white-text">Une bosse sur votre carrosserie ? Contactez-nous !</h1>
         </div>
       </div>
-
-
+      <?php
+        if (isset($_SESSION['contact_send_error'])) {
+      ?>
       <div class="row">
-        <div class="input-field col s4">
-          <input name="adresse" placeholder="5 Rue Quelque Part" type="text" class="validate" class="white-text">
-          <label for="disabled">Adresse (où vous souhaitez l'intervention)</label>
-        </div>
-        <div class="input-field col s4">
-          <input name="ville" placeholder="Ex: Paris" type="text" class="validate" class="white-text">
-          <label for="disabled">Ville</label>
-        </div>
-        <div class="input-field col s4">
-          <input name="codepostal" placeholder="Ex: 75000" type="text" class="validate" class="white-text">
-          <label for="disabled">Code postal</label>
+        <div class="container center-align">
+          <p class="red-text">
+            <?php echo($_SESSION['contact_send_error']);
+            unset($_SESSION['contact_send_error']); ?>
+          </p>
         </div>
       </div>
-
-
-      <div class="row">
-        <div class="input-field col s6">
-          <input name="telephone" placeholder="Ex: 06 07 08 09 10" type="text" class="validate" class="white-text">
-          <label for="disabled">Télephone</label>
-        </div>
-        <div class="input-field col s6">
-          <input name="email" placeholder="Ex: Paul.dubois@email.mail" type="text" class="validate" class="white-text">
-          <label for="disabled">Email</label>
-        </div>
-      </div>
-
-
-      <div class="row">
-        <div class="input-field col s6">
-          <input name="voiture" placeholder="Ex: Renault (Initial Paris)" type="text" class="validate" class="white-text">
-          <label for="disabled">Voiture</label>
-        </div>
-        <form action="#">
-          <div class="file-field input-field col s6">
-            <div class="btn">
-              <span>Envoyer image</span>
-              <input type="file" multiple>
-            </div>
-            <div class="file-path-wrapper">
-              <input class="file-path validate" type="text" placeholder="Charger une ou plusieurs images">
-            </div>
-          </div>
-        </form>
-      </div>
-
-      
-      <div class="row">
-        <form class="col s12">
+      <?php }; ?>
+      <form action="../content/contactsend.php" method="post">
+        <div class="container">
           <div class="row">
-            <div class="input-field col s12">
-              <i class="material-icons prefix white-text">mode_edit</i>
-              <textarea id="icon_prefix2" class="materialize-textarea"></textarea>
-              <label for="icon_prefix2">Message/description de la bosse</label>
+            <div class="input-field col s6">
+              <input name="nom" placeholder="Ex: DUBOIS" id="first_name" type="text" class="validate" class="white-text">
+              <label for="first_name">Nom</label>
+            </div>
+            <div class="input-field col s6">
+              <input name="prenom" placeholder="Ex: Paul" id="last_name" type="text" class="validate" class="white-text">
+              <label for="last_name">Prénom</label>
             </div>
           </div>
-        </form>
-      </div>
-
-      <button class="btn waves-effect waves-light btn-large" type="submit" name="action">Envoyer
-        <i class="material-icons right">send</i>
-      </button>
-
-    <br />
-    </div>
-    </form>
-
-    <?php include '../content/footer.php' ?>
+          <div class="row">
+            <div class="input-field col s4">
+              <input name="adresse" placeholder="5 Rue Quelque Part" type="text" class="validate" class="white-text">
+              <label for="disabled">Adresse (où vous souhaitez l'intervention)</label>
+            </div>
+            <div class="input-field col s4">
+              <input name="ville" placeholder="Ex: Paris" type="text" class="validate" class="white-text">
+              <label for="disabled">Ville</label>
+            </div>
+            <div class="input-field col s4">
+              <input name="codepostal" placeholder="Ex: 75000" type="text" class="validate" class="white-text">
+              <label for="disabled">Code postal</label>
+            </div>
+          </div>
+          <div class="row">
+            <div class="input-field col s6">
+              <input name="telephone" placeholder="Ex: 06 07 08 09 10" type="text" class="validate" class="white-text">
+              <label for="disabled">Télephone</label>
+            </div>
+            <div class="input-field col s6">
+              <input name="email" placeholder="Ex: Paul.dubois@email.mail" type="text" class="validate" class="white-text">
+              <label for="disabled">Email</label>
+            </div>
+          </div>
+          <div class="row">
+            <div class="input-field col s6">
+              <input name="voiture" placeholder="Ex: Renault (Initial Paris)" type="text" class="validate" class="white-text">
+              <label for="disabled">Voiture</label>
+            </div>
+            <form action="#">
+              <div class="file-field input-field col s6">
+                <div class="btn">
+                  <span>Envoyer image</span>
+                  <input type="file" multiple>
+                </div>
+                <div class="file-path-wrapper">
+                  <input class="file-path validate" type="text" placeholder="Charger une ou plusieurs images">
+                </div>
+              </div>
+            </form>
+          </div>
+          <div class="row">
+            <form class="col s12">
+              <div class="row">
+                <div class="input-field col s12">
+                  <i class="material-icons prefix white-text">mode_edit</i>
+                  <textarea id="icon_prefix2" class="materialize-textarea"></textarea>
+                  <label for="icon_prefix2">Message/description de la bosse</label>
+                </div>
+              </div>
+            </form>
+          </div>
+          <button class="btn waves-effect waves-light btn-large" type="submit" name="action">Envoyer
+            <i class="material-icons right">send</i>
+          </button>
+          <br />
+        </div>
+      </form>
+      <?php include '../content/footer.php' ?>
     </div>
     <br>
     <script src="../../js/jquery.min.js" charset="utf-8"></script> <!-- je link le jquery -->
     <script type="text/javascript" src="../../js/materialize.min.js" charset="utf-8"></script> <!-- j'utilise du javascript deja initialiser avec materialize -->
-
     <script>
-    $(document).ready(function() {
-      $('input#input_text, textarea#textarea2').characterCounter();
-    });
+      $(document).ready(function() {
+        $('input#input_text, textarea#textarea2').characterCounter();
+      });
     </script>
-
   </body>
 </html>
--- code/php/page/index.php
+++ code/php/page/index.php
@@ -1,142 +1,100 @@
 <!DOCTYPE html>
-<html lang="en" dir="ltr">
-
-<head>
-  <meta charset="utf-8">
-  <meta name="description" content=""> <!-- DEMANDER UN TEXTE A MON PERE AFIN DE CREER LE SEO -->
-  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"> <!-- je link les icones de materialize si besion neccessaire -->
-  <link rel="stylesheet" href="../../css/style.css"> <!-- je link mon css afin de chager les parametre de ma page -->
-  <link rel="stylesheet" href="../../css/materialize.min.css" media="screen,projection" /> <!-- j'utilise materialize pour mon site -->
-  <link rel="stylesheet" href="../../css/flexboxgrid.min.css" media="screen,projection" /> <!-- j'utilise FlexBox Grid (en ayant modifier les fonction row par rowFlex et col par colFlex) pour mon site -->
-
-  <title><img class="logo" src="../../image/logo.png" alt="">Debossauto.fr</title>
-</head>
-
-<body>
-
-  <header>
-    <?php $dep = ''; ?>
-    <?php include '../content/nav.php' ?>
-  </header>
-
-  <div class="back">
-    <div class="titre">
-      <h1>Debossauto</h1>
-      <h2>24 ans d'expérience a votre disposition</h2>
-    </div>
-  </div>
-
-
-
-  <div class='espace'></div>
-  <div class="textAcceuil">
-    <h2 class="center black-text">Qui sommes nous ?</h2>
-
-    <div class="row">
-      <div class="container center-align col s10 offset-s1 l8 offset-l2">
-        <p class="bold">Grâce à la technique du débosselage sans peinture, nous sommes un peu des kinésithérapeutes de l'automobile !</p>
-
-        <p>DEBOSSAUTO est une société française créée en 2003 par deux collaborateurs, spécialistes de la technique du débosselage sans peinture.</br>
-          Nous justifions d'une expérience de plus de dix ans dans le métier.</p>
-
-        <p>Debossauto intervient auprès des particuliers et des professionnels de l'automobile, qui vont des marques les plus courantes aux véhicules de prestiges.</p>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="description" content="">
+    <!-- DEMANDER UN TEXTE A MON PERE AFIN DE CREER LE SEO -->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <!-- je link les icones de materialize si besion neccessaire -->
+    <link rel="stylesheet" href="../../css/style.css">
+    <!-- je link mon css afin de chager les parametre de ma page -->
+    <link rel="stylesheet" href="../../css/materialize.min.css" media="screen,projection" />
+    <!-- j'utilise materialize pour mon site -->
+    <link rel="stylesheet" href="../../css/flexboxgrid.min.css" media="screen,projection" />
+    <!-- j'utilise FlexBox Grid (en ayant modifier les fonction row par rowFlex et col par colFlex) pour mon site -->
+    <title>Debossauto | Debossauto.fr</title> <!-- Je donne un titre a mon site -->
+    <meta name="description" content="Debossauto est une société française créée en 2003 par deux collaborateurs, spécialistes de la technique du débosselage sans peinture."> <!-- Ainsi qu'une description -->
+    <meta name="keywords" content="Debossauto , debosselage , dsp , automobile , technique">
+    <link rel="shortcut icon" href="../../../assets/logo.png"> <!-- j'ajoute un logo sur l'onglet du site  -->
+  </head>
+  <body>
+    <header>
+      <?php $dep = ''; ?>
+      <?php include '../content/nav.php' ?> <!-- on importe la barre de navigation  -->
+    </header>
+    <div class="back"> <!-- on importe le diaporama créer dans le fichier style.css -->
+      <div class="titre">
+        <h1>Debossauto</h1>
+        <h2>24 ans d'expérience à votre disposition</h2>
       </div>
     </div>
-  </div>
-
-  <div class="container">
-    <div class="cardBorder">
-      <div class="rowFlex">
-        <div class="colFlex-xs-12 colFlex-md-6">
-          <div class="card-image">
-            <img class='center' width="80%" src="../../../assets/morgan5.jpg">
+    <div class='espace'></div> <!-- On utilise une balise pour espacer nos différentes parties -->
+      <div class="container">
+        <div class="textAcceuil">
+          <h2 class="center black-text">Qui sommes-nous ?</h2>
+          <div class="row">
+            <div class="container center-align col s10 offset-s1 l8 offset-l2">
+              <p class="bold">Grâce à la technique du débosselage sans peinture, nous sommes un peu des kinésithérapeutes de l'automobile !</p>
+              <p>DEBOSSAUTO est une société française créée en 2003 par deux collaborateurs, spécialistes de la technique du débosselage sans peinture.</br>
+                Nous justifions d'une expérience de plus de dix ans dans le métier.
+              </p>
+              <p>Debossauto intervient auprès des particuliers et des professionnels de l'automobile, qui vont des marques les plus courantes aux véhicules de prestiges.</p>
           </div>
         </div>
-        <div class="colFlex-xs-12 colFlex-md-6" style="padding:15px">
-          <div class="rowFlex" style="height: 90%">
-            <div class="colFlex-xs-12">
-              <h4 class="center-align">Qu'est ce que le D.S.P</h4>
-              <p>A l’origine, cette technique a été mise au point, par les constructeurs automobiles, pour réparer des défauts légers, causé par les nombreuses manipulations des véhicules, au cours de leurs fabrications. On appelait ces techniciens, des tôliers sur laques.
-                Cette technique artisanale très pointue a ensuite évolué, pour devenir un métier à part entière.
-                En France, dans les années 1990, les premiers techniciens spécialisés ont développés ce service auprès des professionnels de l’automobile.
-                Aujourd’hui tout le monde peut profiter de ce service.</p>
+    </div>
+        <div class="cardBorder">
+          <div class="rowFlex">
+              <div class="colFlex-xs-12 colFlex-md-6">
+                <div class="card-image">
+                    <img class='center' width="80%" src="../../../assets/morgan5.jpg">
+                </div>
+              </div>
+              <div class="colFlex-xs-12 colFlex-md-6" style="padding:15px">
+                <div class="rowFlex" style="height: 90%">
+                    <div class="colFlex-xs-12">
+                      <h4 class="center-align">Qu'est ce que le D.S.P. ?</h4>
+                      <p>À l’origine, cette technique a été mise au point par les constructeurs automobiles, pour réparer des défauts légers, causés par les nombreuses manipulations des véhicules, au cours de leurs fabrications. On appelait ces techniciens "des tôliers sur laques".
+                        Cette technique artisanale très pointue a ensuite évolué, pour devenir un métier à part entière.
+                        En France, dans les années 1990, les premiers techniciens spécialisés ont développé ce service auprès des professionnels de l’automobile.
+                        Aujourd’hui, tout le monde peut profiter de ce service.
+                      </p>
+                    </div>
+                  </div>
+                  <div class="rowFlex bottom-xs center-xs" style="height: 10%">
+                    <div class="colFlex-xs-12">
+                      <a class="waves-effect waves-light btn" href="../page/dsp.php">Voir plus de détails</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="cardBorder">
+              <div class="row">
+                <div class="col s12 m12 l4">
+                  <h3 class="bold"> Avant : </h3>
+                  <img width="100%" src="../../../assets/avant1.jpg" alt="img dsp bosse av">
+                </div>
+                <div class="col s12 m12 l4">
+                  <h3 class="bold">Nos services</h3>
+                  <p>Chez Debossauto, pas besoin de vous déplacer :</br>
+                    Le technicien intervient sur votre domicile ou sur votre lieu de travail, dans la limite de son périmètre d'intervention :</br>
+                    Départements 60, 75, 78, 92, 93 et 95. (nous consulter pour les secteurs limitrophes)
+                  </p>
+                  <p>Pour une intervention, vous pouvez nous contacter au 06.07.65.16.80 ou en cliquant sur "<a href="../page/contact.php">Nous contacter</a>". Nous vous répondrons dans les meilleurs délais.</p>
+                </div>
+                <div class="col s12 m12 l4">
+                  <h3 class="bold">Après :</h3>
+                  <img width="100%" src="../../../assets/apres1.jpg" alt="img dsp bosse ap">
+                </div>
+              </div>
             </div>
           </div>
-          <div class="rowFlex bottom-xs center-xs" style="height: 10%">
-            <div class="colFlex-xs-12">
-              <a class="waves-effect waves-light btn" href="../page/dsp.php">Voir Plus De Détails</a>
-            </div>
+          <h4 class="center">Reportage 2007 sur Debossauto</h4>
+          <div class="row center video">
+            <video controls="controls">
+              <source src="../../../assets/Debossauto_300_cut.mp4">
+            </video>
           </div>
-        </div>
-      </div>
-    </div>
-  </div>
-
-
-  <div class="container">
-    <div class="cardBorder">
-      <div class="row">
-        <div class="col s12 m12 l4">
-          <h3 class="bold"> Avant: </h3>
-          <img width="100%" src="../../../assets/avant1.jpg" alt="img dsp bosse av">
-        </div>
-        <div class="col s12 m12 l4">
-          <h3 class="bold">Nos Services</h3>
-          <p></br>A l’origine, cette technique a été mise au point, par les constructeurs automobiles, pour réparer des défauts légers, causé par les nombreuses manipulations des véhicules, au cours de leurs fabrications. On appelait ces techniciens, des tôliers sur laques.
-            Cette technique artisanale très pointue a ensuite évolué, pour devenir un métier à part entière.
-            En France, dans les années 1990, les premiers techniciens spécialisés ont développés ce service auprès des professionnels de l’automobile.
-            Aujourd’hui tout le monde peut profiter de ce service.</p>
-          <p>Chez Débossauto, pas besoin de vous déplacer:</br>
-            Le techniciens intervient sur votre domicile ou sur votre lieu de travaille, dans la limite de son perimètre d'intervention:</br>
-            Département du 92, 95 et du 78 (nous consulter pour les secteurs limitrophes)</p>
-          <p>Pour une intervention, vous pouvez nous contacter au 06.07.65.16.80 ou en cliquant sur "<a href="../page/contact.php">Nous contacter</a>". Nous vous repondrons dans les meilleur délais.</p>
-          <a class="waves-effect waves-light btn col s8 offset-s2" href='dsp.php'><i class="material-icons left">cloud</i>Voir Plus De Détails</a>
-        </div>
-        <div class="col s12 m12 l4">
-          <h3 class="bold">Apres:</h3>
-          <img width="100%" src="../../../assets/apres1.jpg" alt="img dsp bosse ap">
-        </div>
-      </div>
-    </div>
-  </div>
-<div class="row center video" style="">
-  <video controls="controls">
-    <source src="../../../assets/Debossauto_300_cut.mp4">
-  </video>
-  </div>
-
-  <?php include '../content/footer.php' ?>
-
-
-  <script src="../../js/jquery.min.js" charset="utf-8"></script> <!-- je link le jquery -->
-  <script type="text/javascript" src="../../js/materialize.min.js" charset="utf-8"></script> <!-- j'utilise du javascript deja initialiser avec materialize -->
-  <script type="text/javascript">
-    $(document).ready(function() {
-      $('.parallax').parallax();
-    });
-    $(document).ready(function() {
-      $('.carousel').carousel();
-    });
-    $(document).ready(function() {
-      $('.materialboxed').materialbox();
-    });
-    $('.carousel.carousel-slider').carousel({
-      fullWidth: true
-    });
-
-
-    var instance = M.Carousel.init({
-      fullWidth: true,
-      indicators: true
-    });
-
-    // Or with jQuery
-
-    $('.carousel.carousel-slider').carousel({
-      fullWidth: true,
-      indicators: true
-    });
-  </script>
-</body>
-
+          <?php include '../content/footer.php' ?> <!-- on importe le footer -->
+  </body>
 </html>
